4e33e7e3ea6ab37479cecf834a1d50912e9c53cf
Connor Burk <cburk0709@gmail.com>
2021-11-03 12:13:31 -0700

    commit to pull again

--- app/src/main/java/com/example/gameframework/Domino/players/DominoComputerPlayers1.java
+++ app/src/main/java/com/example/gameframework/Domino/players/DominoComputerPlayers1.java
@@ -1,11 +1,14 @@
 package com.example.gameframework.Domino.players;
 
 import com.example.gameframework.Domino.infoMessage.Domino;
+import com.example.gameframework.Domino.infoMessage.DominoGameState;
 import com.example.gameframework.Domino.infoMessage.MoveInfo;
+import com.example.gameframework.Domino.infoMessage.PlayerInfo;
 import com.example.gameframework.game.GameFramework.infoMessage.GameInfo;
 import com.example.gameframework.game.GameFramework.players.GameComputerPlayer;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class DominoComputerPlayers1 extends GameComputerPlayer {
     private int score;
@@ -27,5 +30,15 @@ public class DominoComputerPlayers1 extends GameComputerPlayer {
 
     @Override
     protected void receiveInfo(GameInfo info) {
+        DominoGameState gameState = new DominoGameState((DominoGameState) info);
+        ArrayList<MoveInfo> legalMoves = gameState.getPlayerInfo()[playerNum].getLegalMoves();
+        int size = legalMoves.size();
+
+        Random rand = new Random();
+        int randomInt = rand.nextInt( size - 1);
+
+        MoveInfo move = legalMoves.get(randomInt);
+        //game.sendAction();
+
     }
 }
