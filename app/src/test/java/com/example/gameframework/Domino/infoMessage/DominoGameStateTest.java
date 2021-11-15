3896635a065f795abacac9d7faf10b07b0d41142
rajan23-git <rajan23@up.edu>
2021-11-15 10:43:10 -0800

    changed J unit test for firstmove and enhanced it

--- app/src/test/java/com/example/gameframework/Domino/infoMessage/DominoGameStateTest.java
+++ app/src/test/java/com/example/gameframework/Domino/infoMessage/DominoGameStateTest.java
@@ -11,60 +11,117 @@ public class DominoGameStateTest {
     @Test
     public void testFirstMove() {
 
-        DominoGameState game_state= new DominoGameState();
+        DominoGameState game_state = new DominoGameState();
         //setting number of players to 4
-game_state.setNumPlayersStart(4);
+        game_state.setNumPlayersStart(4);
 
         game_state.startRound(true);
         //to randomize the cards dealt to each player
 
-        int playerWithMax=-1;//index of player array with maximum weighted domino
-        int dominoIndexMax=-1;// index of player hand with maximum weighted domino
-        int maxDominoWeight=-1; //maximum weight of domino
+        int playerWithMax = -1;//index of player array with maximum weighted domino
+        int dominoIndexMax = -1;// index of player hand with maximum weighted domino
+        int maxDominoWeight = -1; //maximum weight of domino
 
 
-        PlayerInfo [] players=game_state.getPlayerInfo();
-        int numPlayers=players.length;
+        PlayerInfo[] players = game_state.getPlayerInfo();
+        int numPlayers = players.length;
 //outer for-loop walks through playerinfo objects in players array
-        for(int i=0;i<numPlayers;i++){
-            ArrayList<Domino> playerHand= players[i].getHand();
+        for (int i = 0; i < numPlayers; i++) {
+            ArrayList<Domino> playerHand = players[i].getHand();
             //inner for-loop walks through the dominos in the hands of each player
-            for(int j=0;j<playerHand.size();j++){
+            for (int j = 0; j < playerHand.size(); j++) {
                 //if the  maximum weight is smaller than the current domino's wieght,
-                if(playerHand.get(j).getWeight()>maxDominoWeight){
-                    playerWithMax=i;//set with current player
-                    dominoIndexMax=j;//set with current playerHand index
-                    maxDominoWeight=playerHand.get(j).getWeight();//set with new maximum weight
+                if (playerHand.get(j).getWeight() > maxDominoWeight) {
+                    playerWithMax = i;//set with current player
+                    dominoIndexMax = j;//set with current playerHand index
+                    maxDominoWeight = playerHand.get(j).getWeight();//set with new maximum weight
                 }
-
 
 
             }
 
 
-
         }
 // expected first move data calculated from the for loop above
-        int [] expectedFirstMove= new int [2];
-        expectedFirstMove[0]=playerWithMax;
-        expectedFirstMove[1]=dominoIndexMax;
-//unexpected data
-        int [] notExpectedFirstMove= new int [2];
-        notExpectedFirstMove[0]=playerWithMax-1;
-        notExpectedFirstMove[1]=dominoIndexMax-1;
+        int[] expectedFirstMove = new int[2];
+        expectedFirstMove[0] = playerWithMax;
+        expectedFirstMove[1] = dominoIndexMax;
 
-        assertArrayEquals(expectedFirstMove, game_state.firstMove());
-        assertNotEquals(notExpectedFirstMove[0],game_state.firstMove()[0]);
-        assertNotEquals(notExpectedFirstMove[1],game_state.firstMove()[1]);
+        int[] actualFirstMove = new int[2];
+        actualFirstMove = game_state.firstMove();
+//expected first move compared to actual return value from firstMove() function
+        assertArrayEquals(expectedFirstMove, actualFirstMove);
+//re-starting the round so that shuffled Dominos to each player are different
+        //therefore calculating the first move again based on different player hands
+        //and checking if they match the actual first move return value
+        game_state.startRound(true);
+
+        playerWithMax = -1;
+        dominoIndexMax = -1;
+        maxDominoWeight = -1;
+
+        players = game_state.getPlayerInfo();
+        numPlayers = players.length;
+        for (int i = 0; i < numPlayers; i++) {
+            ArrayList<Domino> playerHand = players[i].getHand();
+            for (int j = 0; j < playerHand.size(); j++) {
+                if (playerHand.get(j).getWeight() > maxDominoWeight) {
+                    playerWithMax = i;
+                    dominoIndexMax = j;
+                    maxDominoWeight = playerHand.get(j).getWeight();
+                }
 
 
+            }
 
 
-
-
+        }
+        expectedFirstMove[0] = playerWithMax;
+        expectedFirstMove[1] = dominoIndexMax;
+        actualFirstMove = game_state.firstMove();
+        assertArrayEquals(expectedFirstMove, actualFirstMove);
 
 
     }
+
+    @Test
+    public void testSetTurnId() {
+        DominoGameState game_state = new DominoGameState();
+        game_state.setNumPlayersStart(4);
+        //4 players in this game
+        int initialTurnId = game_state.getTurnID();
+
+        game_state.setTurnID();
+        //comparing the turn ID after set turn ID to what it should be
+
+        assertEquals((initialTurnId + 1) % 4, game_state.getTurnID());
+        game_state.setTurnID();
+        game_state.setTurnID();
+        assertEquals((initialTurnId + 3) % 4, game_state.getTurnID());
+        game_state.setTurnID();
+        game_state.setTurnID();
+
+        assertNotEquals((initialTurnId + 5) % 9, game_state.getTurnID());
+
+
+    }
+
+    @Test
+    public void testStartRound() {
+        DominoGameState game_state = new DominoGameState();
+        game_state.setNumPlayersStart(3);
+        game_state.startRound(true);
+        //number of players is set to 3 and random dominos shuffled to each players hand
+
+        for (int i = 0; i < game_state.getPlayerInfo().length; i++) {
+            //checking to see if each player's hand consists of 5 dominos
+            assertEquals(5, game_state.getPlayerInfo()[i].getHand().size());
+
+        }
+
+    }
+
+
 }
 
 
