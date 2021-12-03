ae946596f86b08159c89ab733cbc25fe6c588afc
rajan23-git <rajan23@up.edu>
2021-12-03 11:51:00 -0800

    added more comments to samrt computer player

--- app/src/main/java/com/example/gameframework/Domino/players/SmartComputerPlayer.java
+++ app/src/main/java/com/example/gameframework/Domino/players/SmartComputerPlayer.java
@@ -25,80 +25,78 @@ public class SmartComputerPlayer extends GameComputerPlayer {
     public SmartComputerPlayer(String name) {
         super(name);
     }
+
     int row;
     int col;
     int dominoIndex;
 
     @Override
     protected void receiveInfo(GameInfo info) {
-        if (info instanceof NotYourTurnInfo){
+        //if not turn do nothing
+        if (info instanceof NotYourTurnInfo) {
             return;
         }
-        if (info instanceof IllegalMoveInfo){
+        //if illegal move
+        if (info instanceof IllegalMoveInfo) {
 
-            game.sendAction(new DominoMoveAction(this, row,col,dominoIndex));
+            game.sendAction(new DominoMoveAction(this, row, col, dominoIndex));
             Logger.log("I", "Illegal move, resending prev. action");
             return;
         }
 
-
-
         DominoGameState gameStateObj = new DominoGameState((DominoGameState) info);
-
-        if (gameStateObj.isGameBlocked()){
+//if game blocked send game blocked action
+        if (gameStateObj.isGameBlocked()) {
             game.sendAction(new DominoGameBlockedAction(this));
         }
-
-        if (gameStateObj.getPlayerInfo()[playerNum].getHand().size() == 0){
-            game.sendAction(new DominoPlacedAllPiecesAction(this,name));
+//if player placed all pieces
+        if (gameStateObj.getPlayerInfo()[playerNum].getHand().size() == 0) {
+            game.sendAction(new DominoPlacedAllPiecesAction(this, name));
         }
 
         //if player doesn't have legal move, draw until there is a legal move. If boneyard is empty
         //skip turn
-        if(gameStateObj.getPlayerInfo()[playerNum].getLegalMoves().size() == 0 &&
-                gameStateObj.getBoneyard().size() == 0)
-        {
+        if (gameStateObj.getPlayerInfo()[playerNum].getLegalMoves().size() == 0 &&
+                gameStateObj.getBoneyard().size() == 0) {
             sleep(1);
             game.sendAction(new DominoSkipAction(this));
             return;
         }
-        if(gameStateObj.getPlayerInfo()[playerNum].getLegalMoves().size() == 0)
-        {
+        if (gameStateObj.getPlayerInfo()[playerNum].getLegalMoves().size() == 0) {
             game.sendAction(new DominoDrawAction(this));
             return;
         }
-       ArrayList<MoveInfo> playerlegalMoves= gameStateObj.getPlayerInfo()[playerNum].getLegalMoves();
-        int maxPoints=-999;
-        int maxMoveIndex=-999;
+        //getting legal moves of smart computer player
+        ArrayList<MoveInfo> playerlegalMoves = gameStateObj.getPlayerInfo()[playerNum].getLegalMoves();
+        int maxPoints = -999;
+        int maxMoveIndex = -999;
+//for loop walks through all the legal moves of smart computer player
+        for (int i = 0; i < playerlegalMoves.size(); i++) {
+            //temporary state initialized from current state of game
+            DominoGameState tempGameState = new DominoGameState(gameStateObj);
+//row of legal move
+            row = (playerlegalMoves.get(i)).getRow();
+            //column of legal move
+            col = (playerlegalMoves.get(i)).getCol();
+            //get domino index of move
+            dominoIndex = (playerlegalMoves.get(i)).getDominoIndex();
+            //applying move to temporary game state
+            tempGameState.placePiece(row, col, playerNum, dominoIndex);
 
-        for(int i=0;i<playerlegalMoves.size();i++){
-            DominoGameState tempGameState= new DominoGameState(gameStateObj);
-
-            row=(playerlegalMoves.get(i)).getRow();
-            col=(playerlegalMoves.get(i)).getCol();
-            dominoIndex=(playerlegalMoves.get(i)).getDominoIndex();
-            tempGameState.placePiece(row,col,playerNum,dominoIndex);
-
-            int currentMovePoints=tempGameState.reportScoredPoints(playerNum);
-
-            if(currentMovePoints>maxPoints){
-                maxPoints=currentMovePoints;
-                maxMoveIndex=i;
-
-
+            int currentMovePoints = tempGameState.reportScoredPoints(playerNum);
+//finds the maximum points generated from each move
+            if (currentMovePoints > maxPoints) {
+                maxPoints = currentMovePoints;
+                maxMoveIndex = i;
             }
-
-
-
-
         }
-
-        MoveInfo bestMove=gameStateObj.getPlayerInfo()[playerNum].getLegalMoves().get(maxMoveIndex);
-
+//the best move based on the maxpoints
+        MoveInfo bestMove = gameStateObj.getPlayerInfo()[playerNum].getLegalMoves().get(maxMoveIndex);
 
         Logger.log("i", "Row: " + row + " Col: " + col + " Index: " + dominoIndex);
 
         sleep(1);
+        //sends best move to game
         game.sendAction(new DominoMoveAction(this, bestMove.getRow(), bestMove.getCol(), bestMove.getDominoIndex()));
 
     }
